4410e9c1ea82040e997a176231a6dc623f2766fc
Joel Denning <joeldenning@gmail.com>
2025-04-14 20:47:29 -0600

    Delete unneeded test for component that doesn't do anything

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, view-dogs');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Franklin is a good boy');
   });
 });
